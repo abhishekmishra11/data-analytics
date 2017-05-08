99c62e286e6523cc87a52c99a0d097fd49b82aaa
Jonathan Gessert <jonathan@321webmarketing.com>
2017-05-08 02:29:26 -0400

    cleaned up setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
-mkdir iuu-fishing-model/datasets/kristina_originals/
-mkdir iuu-fishing-model/datasets/kristina_measures/
-mkdir iuu-fishing-model/datasets/kristina_adjusted/
+mkdir iuu-fishing-model/datasets/originals/
+mkdir iuu-fishing-model/datasets/measures/
+mkdir iuu-fishing-model/datasets/adjusted/
 
 cp training-data/data/merged/kristina_*.npz \
-iuu-fishing-model/datasets/kristina_originals/
+iuu-fishing-model/datasets/originals/
+
+python ../vessel-scoring/scripts/add_measures.py \
+datasets/adjusted/kristina_trawl.npz \
+datasets/measures/kristina_trawl.measures.npz
+
+python ../vessel-scoring/scripts/add_measures.py \
+datasets/adjusted/kristina_ps.npz \
+datasets/measures/kristina_ps.measures.npz
+
+python ../vessel-scoring/scripts/add_measures.py \
+datasets/adjusted/kristina_longliner.npz \
+datasets/measures/kristina_longliner.measures.npz
